71315e1e2b1d093ea5b3bc9acb7b811f3640b22c
Jyotivision <aryaljyoti22.ja@gmail.com>
2021-01-24 07:56:54 -0800

    form data deletion successful

--- delete.php
+++ delete.php
@@ -1,3 +1,13 @@
 <?php 
 	include ('db_co.php');
+	$id=$_GET['id'];
+	$sqll="DELETE FROM users where userID='$id'";
+	$query=mysqli_query($con,$sqll);
+	if ($query) {
+		echo "Successfully deleted";
+	}else{
+		echo "Delete unsuccessful";
+	}
+	mysqli_close($con);
+
  ?>
